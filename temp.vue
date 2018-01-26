9a5af951a79b7ccb3320d3563026130ad24d9013
dbc <dbcdcw25@gmail.com>
2018-01-26 15:41:15 +0800

    1

---
+++ temp.vue
@@ -0,0 +1,12 @@
+<div id="vue-script"><script> var  new Vue({
+    el:"",
+    data:{
+        obj:{},
+        dataSource: {}
+    }, 
+    methods: {},
+    mounted: function () {
+                this.$nextTick(function () {
+                })
+            }
+})</script></div>
