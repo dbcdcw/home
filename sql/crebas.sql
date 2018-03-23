4a97dd6336bbfd5929eddf4c1e03257bd99d50d9
dbcdcw <10557301+dbcdcw@users.noreply.github.com>
2018-03-23 17:58:11 +0800

    123

--- sql/crebas.sql
+++ sql/crebas.sql
@@ -1,35 +1,32 @@
 /*==============================================================*/
 /* DBMS name:      ORACLE Version 11g                           */
-/* Created on:     2018/2/1 17:42:15                            */
+/* Created on:     2018/3/21 10:22:55                           */
 /*==============================================================*/
 
 
-drop index IDX_T_LAYOUT_DS_PARENTID;
-
-drop table T_LAYOUT_DATA_SOURSE cascade constraints;
+drop table T_LAYOUT_STRING_TO_METHOD cascade constraints;
 
 /*==============================================================*/
-/* Table: T_LAYOUT_DATA_SOURSE                                  */
+/* Table: T_LAYOUT_STRING_TO_METHOD                             */
 /*==============================================================*/
-create table T_LAYOUT_DATA_SOURSE 
+create table T_LAYOUT_STRING_TO_METHOD 
 (
    ID                   NUMBER(19)           not null,
    ND_CODE              VARCHAR2(20)         not null,
    ND_NAME              VARCHAR2(50)         not null,
    ND_TYPE              VARCHAR2(10)         not null,
    PARENT_ID            NUMBER(19),
-   DATA_SOURSE_NAME     VARCHAR2(4000),
-   BIND_ND_CODE         VARCHAR2(4000),
-   constraint PK_T_LAYOUT_DATA_SOURSE primary key (ID)
+   CREATOR              VARCHAR2(50)         not null,
+   CREATE_TIME          TIMESTAMP            not null,
+   MODIFIER             VARCHAR2(50),
+   UPDATE_TIME          TIMESTAMP,
+   COMPONENT_TYPE       VARCHAR2(3),
+   TYPE                 VARCHAR2(50),
+   STRING_VALUE         VARCHAR2(50),
+   TIGGER_NAME          VARCHAR2(50),
+   constraint PK_T_LAYOUT_STRING_TO_METHOD primary key (ID)
 );
 
-comment on table T_LAYOUT_DATA_SOURSE is
-'布局数据源表';
-
-/*==============================================================*/
-/* Index: IDX_T_LAYOUT_DS_PARENTID                              */
-/*==============================================================*/
-create index IDX_T_LAYOUT_DS_PARENTID on T_LAYOUT_DATA_SOURSE (
-   PARENT_ID ASC
-);
+comment on table T_LAYOUT_STRING_TO_METHOD is
+'字符串值转方法';
 
